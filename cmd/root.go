e2d24224598c8188ed95bdf6a10832b4d573275b
Johnie Hjelm <johnie@hjelm.im>
2024-09-14 21:35:29 +0200

    feat: Add URL flag to print JIRA ticket URL
    This commit adds a new flag, `--url` or `-u`, to the root command. When this flag is provided, the command will print the JIRA ticket URL based on the configured JIRA URL and the provided ticket number. Additionally, if the `--copy` or `-c` flag is also provided, the URL will be copied to the clipboard. This feature enhances the functionality of the command by allowing users to quickly access the JIRA ticket associated with a specific task.

--- cmd/root.go
+++ cmd/root.go
@@ -4,17 +4,20 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/johnie/gtm/lib/ui"
 	"github.com/johnie/gtm/utils"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var (
 	versionFlag bool
 	copyFlag    bool
 	messageFlag string
+	urlFlag     bool
 )
 
 var rootCmd = &cobra.Command{
@@ -30,9 +33,48 @@ func Execute() error {
 }
 
 func init() {
+	loadConfig()
+
 	rootCmd.Flags().BoolVarP(&versionFlag, "version", "v", false, "Print the version")
 	rootCmd.Flags().BoolVarP(&copyFlag, "copy", "c", false, "Only copy the ticket value")
 	rootCmd.Flags().StringVarP(&messageFlag, "message", "m", "", "Commit message")
+	rootCmd.Flags().BoolVarP(&urlFlag, "url", "u", false, "Print Ticket URL")
+}
+
+func loadConfig() {
+	viper.SetConfigName(".gtmconfig")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath("$HOME")
+
+	viper.SetDefault("core.jira_url", "")
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("Could not get home directory: %v\n", err)
+		return
+	}
+
+	configPath := filepath.Join(homeDir, ".gtmconfig")
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		createDefaultConfig(configPath)
+	}
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		fmt.Printf("Could not read config file: %v\n", err)
+	}
+}
+
+func createDefaultConfig(configPath string) {
+	defaultConfig := []byte(`[core]
+jira_url = "https://jira.example.com/browse/"
+`)
+	err := os.WriteFile(configPath, defaultConfig, 0644)
+	if err != nil {
+		fmt.Printf("Could not create default config file: %v\n", err)
+	} else {
+		fmt.Printf("Default config file created at %s\n", configPath)
+	}
 }
 
 func run(cmd *cobra.Command, args []string) error {
@@ -56,6 +98,21 @@ func run(cmd *cobra.Command, args []string) error {
 		reader := bufio.NewReader(os.Stdin)
 		ticketInput, _ := reader.ReadString('\n')
 		ticket = strings.TrimSpace(ticketInput)
+	}
+
+	if urlFlag {
+		jiraURL := viper.GetString("core.jira_url")
+		if jiraURL == "" {
+			return fmt.Errorf("JIRA URL not set in config")
+		}
+		fmt.Println(jiraURL + ticket)
+		if copyFlag {
+			if err := Utils.Copy(jiraURL + ticket); err != nil {
+				return fmt.Errorf("error copying to clipboard: %w", err)
+			}
+			fmt.Println(ui.PrePrendCheckmark(ui.TicketStyle(jiraURL + ticket)))
+		}
+		return nil
 	}
 
 	if copyFlag {
